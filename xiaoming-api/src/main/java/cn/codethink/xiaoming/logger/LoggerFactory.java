f87624259e9184f26b0559003b863ecb4c6a08f7
Chuanwise <chuanwise@qq.com>
2022-03-20 01:10:49 +0800

    update build.gradle

--- xiaoming-api/src/main/java/cn/codethink/xiaoming/logger/LoggerFactory.java
+++ xiaoming-api/src/main/java/cn/codethink/xiaoming/logger/LoggerFactory.java
@@ -3,18 +3,31 @@ package cn.codethink.xiaoming.logger;
 import cn.codethink.common.util.Maps;
 import cn.codethink.common.util.Preconditions;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 全局日志工厂
+ *
  * @author Chuanwise
  */
 public abstract class LoggerFactory {
-
-    /** 保存现有日志的哈希表 */
+    
+    /**
+     * 保存现有日志的哈希表
+     */
     private static final Map<String, Logger> LOGGERS = new ConcurrentHashMap<>();
-
+    
+    /**
+     * 获取所有现有的 Logger
+     *
+     * @return 现有的 Logger
+     */
+    public static Map<String, Logger> getLoggers() {
+        return Collections.unmodifiableMap(LOGGERS);
+    }
+    
     /**
      * 获得一个日志
      * @param name 日志名
