3adf842d178be977927157a5ea1eb1c3ff68d238
Chuanwise <chuanwise@qq.com>
2023-09-25 22:57:17 +0800

    feat: add `Event` and its simple implementation `AbstractEvent`

---
+++ xiaoming-api/src/main/java/cn/codethink/xiaoming/event/AbstractEvent.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.event;
+
+/**
+ * <h1>Abstract Event</h1>
+ *
+ * <p>Abstract event is a simple implementation of {@link Event} for
+ * developers to inherit from. </p>
+ *
+ * <p>Not all events must be inherited from this class, but if you are
+ * developing a simple event, it's recommended to inherit from it. </p>
+ *
+ * @see Event
+ * @author Chuanwise
+ */
+public abstract class AbstractEvent
+    implements Event {
+
+    /**
+     * field to record if event is intercepted.
+     */
+    private volatile boolean intercepted = false;
+
+    public final boolean isIntercepted() {
+        return intercepted;
+    }
+
+    public final void setIntercepted(boolean intercepted) {
+        this.intercepted = intercepted;
+    }
+}
---
+++ xiaoming-api/src/main/java/cn/codethink/xiaoming/event/Event.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.event;
+
+/**
+ * <h1>Event</h1>
+ *
+ * <p>Just like many similar implementations, events are something matter,
+ * which will be published when special things happens. </p>
+ *
+ * <p>Event can be intercepted by {@code event.setIntercepted(true)}. After
+ * intercepted, event will not be spread to other event listeners. </p>
+ *
+ * @author Chuanwise
+ */
+public interface Event {
+
+    /**
+     * query if event is intercepted.
+     *
+     * @return if event is intercepted
+     */
+    boolean isIntercepted();
+
+    /**
+     * set if event is intercepted.
+     *
+     * @param cancelled if event is intercepted
+     */
+    void setIntercepted(boolean cancelled);
+}
