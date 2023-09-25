1d2a2e05c3b9251a9e561fe96a1e3db611579762
Chuanwise <chuanwise@qq.com>
2023-09-25 23:05:05 +0800

    feat: add `Component` to manage some low-level functions of a bot

---
+++ xiaoming-api/src/main/java/cn/codethink/xiaoming/component/Component.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.component;
+
+import cn.codethink.xiaoming.Subject;
+
+/**
+ * <h1>Component</h1>
+ *
+ * <p>Component is a set of functions related to a bot directly
+ * and strongly. It always be low-level, such as protocol fixing. </p>
+ *
+ * @author Chuanwise
+ */
+public interface Component
+    extends Subject {
+}
