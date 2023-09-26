82d4dd24a8bf0769f8b65c59a46e8a6645a35747
Chuanwise <chuanwise@qq.com>
2023-09-26 13:26:12 +0800

    feat: add interface `Relation` to represent a connection between bot and others

---
+++ xiaoming-api/src/main/java/cn/codethink/xiaoming/relation/Relation.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.relation;
+
+/**
+ * <h1>Relation</h1>
+ *
+ * <p>Relation is a connection between bot and others. Such as {@link Friend},
+ * {@link Member}, etc. </p>
+ *
+ * @author Chuanwise
+ */
+public interface Relation {
+}
