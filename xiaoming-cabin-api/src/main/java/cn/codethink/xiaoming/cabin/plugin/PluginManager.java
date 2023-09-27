1b26dcc63fbefbade855a257f57de3f3ddd1593e
Chuanwise <chuanwise@qq.com>
2023-09-27 22:23:39 +0800

    feat: add `PluginManager`

---
+++ xiaoming-cabin-api/src/main/java/cn/codethink/xiaoming/cabin/plugin/PluginManager.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.cabin.plugin;
+
+import cn.codethink.xiaoming.cabin.Cabin;
+
+/**
+ * <h1>Plugin Manager</h1>
+ *
+ * <p>Plugin manager manages objects related plugins, such as plugin
+ * instances, scanners, etc. </p>
+ *
+ * @author Chuanwise
+ */
+public interface PluginManager {
+
+    /**
+     * Get cabin.
+     *
+     * @return cabin
+     */
+    Cabin getCabin();
+}
