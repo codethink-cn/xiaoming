be8215f0cea1ba5a4cebd5d498b251482edc5a3f
Chuanwise <chuanwise@qq.com>
2023-09-28 22:37:27 +0800

    refactor: adjusted resources directories of launcher

--- xiaoming-cabin-launcher/src/main/java/cn/codethink/xiaoming/cabin/launcher/LauncherMain.java
+++ xiaoming-cabin-launcher/src/main/java/cn/codethink/xiaoming/cabin/launcher/LauncherMain.java
@@ -16,14 +16,49 @@
 
 package cn.codethink.xiaoming.cabin.launcher;
 
+import cn.codethink.xiaoming.cause.Cause;
+
+import java.io.*;
+
 /**
  * <h1>Launcher Main</h1>
  *
  * @author Chuanwise
  */
 public class LauncherMain {
+
+    private static final String ASCII_ICON_PATH = "META-INF/xiaoming/cabin/launcher/ascii-icon.txt";
+
+    private static void printAsciiIcon() {
+        final ClassLoader classLoader = LauncherMain.class.getClassLoader();
+        final String asciiIconString;
+
+        try (final InputStream inputStream = classLoader.getResourceAsStream(ASCII_ICON_PATH)) {
+            int available = inputStream.available();
+            final byte[] buffer = new byte[available];
+
+            while ((available -= inputStream.read(buffer)) != 0) {
+                // nothing
+            }
+
+            asciiIconString = new String(buffer);
+        } catch (final IOException e) {
+            System.out.println("Fail to load the ascii icon in resource file '" + ASCII_ICON_PATH + "'! ");
+            return;
+        }
+
+        System.out.println(asciiIconString);
+    }
+
     public static void main(String[] args) {
 
-        // TODO: parse arguments
+        printAsciiIcon();
+
+        final Launcher launcher = new Launcher();
+        try {
+            launcher.enable(Cause.of("Launcher main called"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
