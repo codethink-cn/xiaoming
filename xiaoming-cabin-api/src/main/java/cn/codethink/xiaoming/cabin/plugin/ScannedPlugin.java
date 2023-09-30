1572fa9f1753490a657650ccff0ba30fe0324600
Chuanwise <chuanwise@qq.com>
2023-09-30 19:17:43 +0800

    feat: add `ScannedPlugin`

---
+++ xiaoming-cabin-api/src/main/java/cn/codethink/xiaoming/cabin/plugin/ScannedPlugin.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.cabin.plugin;
+
+/**
+ * <h1>Scanned Plugin</h1>
+ *
+ * <p>Scanned plugin is a kind of plugin can be scanned
+ * by plugin scanner. </p>
+ *
+ * @author Chuanwise
+ */
+public interface ScannedPlugin
+        extends Plugin {
+
+    /**
+     * Get scanner.
+     *
+     * @return scanner
+     */
+    PluginScanner getScanner();
+}
