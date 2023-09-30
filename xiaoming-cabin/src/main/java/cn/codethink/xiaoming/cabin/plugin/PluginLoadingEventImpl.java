c4bd2f832ed3d55ba2201d8ed42a82f5702a837a
Chuanwise <chuanwise@qq.com>
2023-09-30 19:18:16 +0800

    feat: added implementation of `PluginLoadingEvent`

---
+++ xiaoming-cabin/src/main/java/cn/codethink/xiaoming/cabin/plugin/PluginLoadingEventImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.cabin.plugin;
+
+import cn.codethink.xiaoming.cabin.Cabin;
+import cn.codethink.xiaoming.cause.Cause;
+import cn.codethink.xiaoming.event.AbstractCancellableEvent;
+import com.google.common.base.Preconditions;
+
+public class PluginLoadingEventImpl
+        extends AbstractCancellableEvent
+        implements PluginLoadingEvent {
+
+    private final Cabin cabin;
+    private final Plugin plugin;
+
+    public PluginLoadingEventImpl(Cabin cabin, Plugin plugin) {
+        Preconditions.checkNotNull(cabin, "Cabin is null! ");
+        Preconditions.checkNotNull(plugin, "Plugin is null! ");
+
+        this.cabin = cabin;
+        this.plugin = plugin;
+    }
+
+    @Override
+    public Cause getCause() {
+        return getDirectCause();
+    }
+
+    @Override
+    public Cabin getCabin() {
+        return cabin;
+    }
+
+    @Override
+    public Plugin getPlugin() {
+        return plugin;
+    }
+}
