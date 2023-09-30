b7e7442d57cba88ee50e7c0f96f9406d08da5b1c
Chuanwise <chuanwise@qq.com>
2023-09-30 16:59:28 +0800

    feat: added `State` and related apis

---
+++ xiaoming-core-api/src/main/java/cn/codethink/xiaoming/state/StateSwitchingEvent.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.state;
+
+import cn.codethink.xiaoming.Subject;
+import cn.codethink.xiaoming.event.CancellableEvent;
+
+/**
+ * <h1>State Switching Event</h1>
+ *
+ * <p>State switching event is event will be published when
+ * the state of something will be changed. </p>
+ *
+ * @author Chuanwise
+ */
+public interface StateSwitchingEvent
+        extends CancellableEvent {
+
+    /**
+     * Get holder.
+     *
+     * @return holder
+     */
+    Stated getHolder();
+
+    /**
+     * Get switcher.
+     *
+     * @return switcher
+     */
+    Subject getSwitcher();
+}
---
+++ xiaoming-core-api/src/main/java/cn/codethink/xiaoming/state/StateType.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.state;
+
+/**
+ * <h1>State Type</h1>
+ *
+ * @author Chuanwise
+ */
+public interface StateType {
+
+    int ERROR_MASK = 1;
+    int DOING_MASK = 1 << 1;
+
+    /**
+     * Query if the state type is error.
+     *
+     * @return if the state type is error
+     */
+    boolean isError();
+
+    /**
+     * Query if the state type is doing.
+     *
+     * @return if the state type is doing
+     */
+    boolean isDoing();
+
+    /**
+     * Get mask.
+     *
+     * @return mask
+     */
+    int getMask();
+}
